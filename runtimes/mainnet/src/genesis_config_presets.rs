ac747da70125ca3d2f3cd4b9dc6fbece4e3134bf
tejas-wl <tejas.gaware@wowlabz.com>
2025-09-17 18:49:35 +0530

    removed some warnings

--- runtimes/mainnet/src/genesis_config_presets.rs
+++ runtimes/mainnet/src/genesis_config_presets.rs
@@ -23,7 +23,7 @@ use crate::{
 	constants::currency::*, AccountId, AssetsConfig,
 	BabeConfig, Balance, BalancesConfig, ElectionsConfig, NominationPoolsConfig,
 	RuntimeGenesisConfig, SessionConfig, SessionKeys, SocietyConfig, StakingConfig,
-	SudoConfig, TechnicalCommitteeConfig, BABE_GENESIS_EPOCH_CONFIG, PDEXMigrationConfig, BeefyConfig
+	SudoConfig, TechnicalCommitteeConfig, BABE_GENESIS_EPOCH_CONFIG, PDEXMigrationConfig,
 };
 pub use pallet_staking::StakerStatus;
 use frame_support::build_struct_json_patch;
@@ -32,7 +32,6 @@ use pallet_im_online::sr25519::AuthorityId as ImOnlineId;
 use sp_authority_discovery::AuthorityId as AuthorityDiscoveryId;
 use sp_consensus_babe::AuthorityId as BabeId;
 use sp_consensus_beefy::ecdsa_crypto::AuthorityId as BeefyId;
-use sp_consensus_beefy::ecdsa_crypto::Public as BeefyPublicId;
 use sp_consensus_grandpa::AuthorityId as GrandpaId;
 use sp_core::{crypto::get_public_from_string_or_panic, sr25519};
 use sp_genesis_builder::PresetId;
