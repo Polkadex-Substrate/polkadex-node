893437dbdb0ecb1b392e6335ec9bee5672010419
tejas-wl <tejas.gaware@wowlabz.com>
2026-04-30 22:55:08 +0530

    Add TokenGatewayStorageKeyMigration

--- runtimes/mainnet/src/lib.rs
+++ runtimes/mainnet/src/lib.rs
@@ -170,7 +170,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // and set impl_version to 0. If only runtime
     // implementation changes and behavior does not, then leave spec_version as
     // is and increment impl_version.
-    spec_version: 381,
+    spec_version: 383,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
@@ -2947,6 +2947,7 @@ type Migrations = (
     migrations::IdentityStorageVersionMigration<Runtime>,
     migrations::ChildBountiesStorageVersionMigration<Runtime>,
     migrations::TokenGatewayLocalAssetsMigration,
+    migrations::TokenGatewayStorageKeyMigration,
     migrations::AssetsStorageMigration,
     // Existing migrations
     // pallet_nomination_pools::migration::versioned::V6ToV7<Runtime>,
