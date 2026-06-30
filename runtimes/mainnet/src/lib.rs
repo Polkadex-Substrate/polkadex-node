792c9341c2beb1bf9bfc579df7b83a62bde7774f
tejas-wl <tejas.gaware@wowlabz.com>
2026-06-30 14:46:00 +0530

    removed redundant migrations and added docs

--- runtimes/mainnet/src/lib.rs
+++ runtimes/mainnet/src/lib.rs
@@ -2964,13 +2964,10 @@ type Migrations = (
     migrations::GrandpaStorageVersionMigration<Runtime>,
     migrations::IdentityStorageVersionMigration<Runtime>,
     migrations::ChildBountiesStorageVersionMigration<Runtime>,
-    migrations::AssetsStorageMigration,
     // Existing migrations
-    // pallet_nomination_pools::migration::versioned::V6ToV7<Runtime>,
     pallet_alliance::migration::Migration<Runtime>,
     pallet_contracts::Migration<Runtime>,
     pallet_identity::migration::versioned::V0ToV1<Runtime, IDENTITY_MIGRATION_KEY_LIMIT>,
-    migrations::ClearIsmpStaleRequests,
 );
 
 type EventRecord = frame_system::EventRecord<
